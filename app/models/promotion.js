7ce6722bec4b1006e78b4eec5eb29b8d341b6fa1
Bode Fuwa <bodefuwa@gmail.com>
2015-04-25 01:28:59 -0400

    added some add ons

--- app/models/promotion.js
+++ app/models/promotion.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: 		DS.attr('string'),
   business: 	DS.belongsTo('business'),
-  promoDate: 	DS.attr('date')
+  promoDate: 	DS.attr('date'),
   createdAt: 	DS.attr('string', {
     defaultValue: function() { return new Date(); }
   })
--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   businessname: DS.attr('string'),
   email: 		DS.attr('string'),
   joinDate: 	DS.attr('string', {
-  	defaultValue: function { return new Date(); }
+  	defaultValue: function() { return new Date(); }
   }),
   business: 	DS.belongsTo('business'),
 });
